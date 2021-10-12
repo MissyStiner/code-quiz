7b7ced565d38badd187d9b1dfdf402f854c0c324
Missy Stiner <missystiner@gmail.com>
2021-10-11 23:47:15 -0500

    css added

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var buttonStartQuiz = document.querySelector("#start-quiz");
+console.log(buttonStartQuiz);
+
+// Commonly used data types DO not include
+// 1. strings
+// 2. booleans no
+// 3. alerts
+// 4. numbers
+
+// The condition in an if/else statment is enclosed with _____.
+// 1. quotes
+// 2. curly brackets
+// 3. parenthesis yes
+// 4. square brackets
+
+// Arrays in JavaScript can be used to store _______.
+// 1. numbers and strings
+// 2. other Arrays
+// 3. booleans
+// 4 all of the above yes
+
+// String values must be enclosed within _____ when being assigned to variables.
+// 1. commas
+// 2. curly brackets ***correct***
+// 3. quotes
+// 4. parenthesis
+
+// A very useful tool used during development and debugging for printing content to the debugger is:
+// 1. JavaScript
+// 2. terminal/bash
+// 3. for loops
+// 4. console.log ***Correct***
+
+// All done!
+// Your final score is 22.
+// Enter initials: {    } [Submit]
+
+// High Scores
+// 1. AB - 22
+// [Go Back] [Clear High Scores]
